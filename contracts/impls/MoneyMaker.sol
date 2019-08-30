1f8def51d3803147231e5189217bb565479b0613
Bryan Chen <xlchen1291@gmail.com>
2019-08-30 16:10:56 +1200

    add money maker todo

---
+++ contracts/impls/MoneyMaker.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+// TODOs:
+// - config params
+// - deposit tokens to lending protocols to make interest
+// - select which lending protocol to use
+// - issue token which will be used to redeem underlying token & interests
+// - handle liquidity requirement
+// - maybe accept USDC & DAI and issue index token
+
+contract MoneyMaker {
+
+}
