9498768b2d70dbb6e68aa51e11bcf481db8141d2
Bryan Chen <xlchen1291@gmail.com>
2019-09-02 14:10:59 +1200

    update liquidity pool tests

--- test/liquidityPool.js
+++ test/liquidityPool.js
@@ -18,57 +18,71 @@ contract('LiquidityPool', accounts => {
     liquidityPool = await LiquidityPool.new(usd.address, helper.fromPip(10), [fToken]);
   });
 
-  it('should be able to set and get spread', async () => {
-    let spread = await liquidityPool.getSpread(badAddress);
-    expect(spread).bignumber.equal(helper.ZERO);
+  describe('spread', () => {
+    it('should get 0 for disabled token', async () => {
+      const spread = await liquidityPool.getSpread(badAddress);
+      expect(spread).bignumber.equal(helper.ZERO);
+    });
+    
+    it('should get default value', async () => {
+      const spread = await liquidityPool.getSpread(fToken);
+      expect(spread).bignumber.equal(helper.fromPip(10));
+    });
 
-    spread = await liquidityPool.getSpread(fToken);
-    expect(spread).bignumber.equal(helper.fromPip(10));
+    it('should be able to set and get new value', async () => {
+      await liquidityPool.setSpread(helper.fromPip(20));
+      const spread = await liquidityPool.getSpread(fToken);
+      expect(spread).bignumber.equal(helper.fromPip(20));
+    });
 
-    await liquidityPool.setSpread(helper.fromPip(20));
-    spread = await liquidityPool.getSpread(fToken);
-    expect(spread).bignumber.equal(helper.fromPip(20));
+    it('requires owner to set spread', async () => {
+      await expectRevert(liquidityPool.setSpread(helper.fromPip(30), { from: badAddress }), helper.messages.onlyOwner);
+    });
   });
 
-  it('requires owner to set spread', async () => {
-    await expectRevert(liquidityPool.setSpread(helper.fromPip(30), { from: badAddress }), helper.messages.onlyOwner);
+  describe('collateral ratio', () => {
+    it('should get 0 for disabled token', async () => {
+      let ratio = await liquidityPool.getCollateralRatio(badAddress);
+      expect(ratio).bignumber.equal(helper.ZERO);
+    });
+
+    it('should get default value', async () => {
+      const ratio = await liquidityPool.getCollateralRatio(fToken);
+      expect(ratio).bignumber.equal(helper.ZERO);
+    });
+
+    it('should be able to set and get new value', async () => {
+      await liquidityPool.setCollateralRatio(helper.fromPercent(20));
+      const ratio = await liquidityPool.getCollateralRatio(fToken);
+      expect(ratio).bignumber.equal(helper.fromPercent(20));
+    });
+
+    it('requires owner to set collateral ratio', async () => {
+      await expectRevert(liquidityPool.setCollateralRatio(helper.fromPercent(30), { from: badAddress }), helper.messages.onlyOwner);
+    });
   });
 
-  it('should be able to set and get collateral ratio', async () => {
-    let ratio = await liquidityPool.getCollateralRatio(badAddress);
-    expect(ratio).bignumber.equal(helper.ZERO);
+  describe('enable token', async () => {
+    it('should be able to enable token', async () => {
+      await liquidityPool.enableToken(fTokenTwo);
 
-    ratio = await liquidityPool.getCollateralRatio(fToken);
-    expect(ratio).bignumber.equal(helper.ZERO);
+      const spread = await liquidityPool.getSpread(fTokenTwo);
+      expect(spread).bignumber.equal(helper.fromPip(10), 'should get spread for enabled token');
+    });
 
-    await liquidityPool.setCollateralRatio(helper.fromPercent(20));
-    ratio = await liquidityPool.getCollateralRatio(fToken);
-    expect(ratio).bignumber.equal(helper.fromPercent(20));
-  });
+    it('should be able to enable token', async () => {
+      await liquidityPool.disableToken(fToken);
+  
+      const spread = await liquidityPool.getSpread(fToken);
+      expect(spread).bignumber.equal(helper.ZERO, 'should get 0 for disabled token');
+    });
 
-  it('requires owner to set collateral ratio', async () => {
-    await expectRevert(liquidityPool.setCollateralRatio(helper.fromPercent(30), { from: badAddress }), helper.messages.onlyOwner);
-  });
+    it('requires owner to enable token', async () => {
+      await expectRevert(liquidityPool.enableToken(fTokenTwo, { from: badAddress }), helper.messages.onlyOwner);
+    });
 
-  it('should be able to enable and disable token', async () => {
-    await liquidityPool.enableToken(fTokenTwo);
-
-    let spread = await liquidityPool.getSpread(fTokenTwo);
-    expect(spread).bignumber.equal(helper.fromPip(10));
-
-    await liquidityPool.disableToken(fTokenTwo);
-
-    spread = await liquidityPool.getSpread(fTokenTwo);
-    expect(spread).bignumber.equal(helper.ZERO);
-
-    await liquidityPool.disableToken(fToken);
-
-    spread = await liquidityPool.getSpread(fToken);
-    expect(spread).bignumber.equal(helper.ZERO);
-  });
-
-  it('requires owner to enable and disable token', async () => {
-    await expectRevert(liquidityPool.enableToken(fTokenTwo, { from: badAddress }), helper.messages.onlyOwner);
-    await expectRevert(liquidityPool.disableToken(fToken, { from: badAddress }), helper.messages.onlyOwner);
+    it('requires owner to disable token', async () => {
+      await expectRevert(liquidityPool.disableToken(fToken, { from: badAddress }), helper.messages.onlyOwner);
+    });
   });
 })
