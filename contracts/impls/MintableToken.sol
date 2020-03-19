93b96dd6383957798a66abef1135949a7eec0b0c
Markus Waas <mail@markuswaas.de>
2020-03-19 13:57:04 +1300

    use moved Ownable.sol contract

--- contracts/impls/MintableToken.sol
+++ contracts/impls/MintableToken.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.6.3;
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
