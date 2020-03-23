35305640b0017829244554a3677e47fd0495f189
Markus Waas <mail@markuswaas.de>
2020-03-23 19:21:27 +1300

    add open/close position and margin calls

---
+++ contracts/interfaces/MarginLiquidityPoolInterface.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.4;
+
+import "./LiquidityPoolInterface.sol";
+
+interface MarginLiquidityPoolInterface is LiquidityPoolInterface {
+    function depositLiquidity(uint256 _realized) external;
+
+    function withdrawLiquidity(uint256 _realized) external;
+}
