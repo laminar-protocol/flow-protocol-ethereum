c08e0ebd748de69b540ebee55f67aa6983a8d84f
Shaopeng Wang <spxwang@gmail.com>
2020-04-15 18:20:02 +1200

    Update web3 provider.

--- test/cucumber/step-definitions/margin.steps.ts
+++ test/cucumber/step-definitions/margin.steps.ts
@@ -1,5 +1,4 @@
 import { Given, TableDefinition, Before } from 'cucumber';
-import ganache from 'ganache-core';
 import Web3 from 'web3';
 import { Account } from 'web3-core';
 import BN from 'bn.js';
@@ -7,7 +6,7 @@ import BN from 'bn.js';
 import abi from '../../../artifacts/development/abi/LiquidityPoolInterface.json';
 import deployment from '../../../artifacts/development/deployment.json';
 
-const web3 = new Web3(ganache.provider() as any);
+const web3 = new Web3('http://localhost:8545');
 
 const poolAddress = deployment['pool'];
 const poolContract = new web3.eth.Contract(abi as any, poolAddress);
@@ -15,38 +14,49 @@ const poolContract = new web3.eth.Contract(abi as any, poolAddress);
 let alice: Account;
 let bob: Account;
 
-Before(async () => {
+Before(() => {
   alice = web3.eth.accounts.create();
   bob = web3.eth.accounts.create();
 });
 
 const accountOf = (name: string): Account => {
-  let accounts: any = {
+  const accounts: any = {
     Alice: alice,
     Bob: bob,
   };
   return accounts[name];
-}
+};
 
 const transfer = async (name: string, amount: BN) => {
   const accounts = await web3.eth.getAccounts();
-  await web3.eth.sendTransaction({ to: accountOf(name).address, from: accounts[0], value: amount});
+  await web3.eth.sendTransaction({
+    to: accountOf(name).address,
+    from: accounts[0],
+    value: amount,
+  });
 };
 
 const parseAmount = (amount: string): BN => {
-  const parsed = amount.replace(' ', '').replace('$', '').replace('_', '');
+  const parsed = amount
+    .replace(' ', '')
+    .replace('$', '')
+    .replace('_', '');
   return web3.utils.toBN(parsed).mul(new BN(10 ** 12));
 };
 
 Given(/accounts/, async (table: TableDefinition) => {
-  await Promise.all(table.rows().map((row) => transfer(row[0], parseAmount(row[1]))));
+  await Promise.all(
+    table.rows().map(row => transfer(row[0], parseAmount(row[1]))),
+  );
 });
 
-Given(/create liquidity pool/, async () => {});
+Given(/create liquidity pool/, () => {});
 
 Given(/deposit liquidity/, async (table: TableDefinition) => {
   for (const [name, amount] of table.rows()) {
-    const calldata = poolContract.methods.depositLiquidity(parseAmount(amount)).encodeABI();
+    const calldata = poolContract.methods
+      .depositLiquidity(parseAmount(amount))
+      .encodeABI();
     const from = accountOf(name);
     const tx = {
       from: from.address,
