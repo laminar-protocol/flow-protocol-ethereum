d8f9ba9d85435eff8e765ab4b9930c40f5a3b8b1
Shaopeng Wang <spxwang@gmail.com>
2020-04-16 14:09:21 +1200

    Update cucubmer directory.

--- cucumber.js
+++ cucumber.js
@@ -1,11 +1,9 @@
-let common = [
-  'test/cucumber/features/**/*.feature', // Specify our feature files
-  '--require-module ts-node/register', // Load TypeScript module
-  '--require test/cucumber/step-definitions/**/*.ts', // Load step definitions
-  '--format progress-bar', // Load custom formatter
-  '--format node_modules/cucumber-pretty' // Load custom formatter
-].join(' ');
-
 module.exports = {
-  default: common
+  default: [
+    'cucumber/features/**/*.feature', // Specify our feature files
+    '--require-module ts-node/register', // Load TypeScript module
+    '--require cucumber/step-definitions/**/*.ts', // Load step definitions
+    '--format progress-bar', // Load custom formatter
+    '--format node_modules/cucumber-pretty', // Load custom formatter
+  ].join(' '),
 };
